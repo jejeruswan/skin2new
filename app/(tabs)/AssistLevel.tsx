7a2618f0e309060844c4e46b88c260882eff2782
JesslynAR <jejegolfeto@gmail.com>
2025-04-16 20:43:42 -0700

    navigation-types

--- app/(tabs)/AssistLevel.tsx
+++ app/(tabs)/AssistLevel.tsx
@@ -5,7 +5,7 @@ import { OptionButton } from "@/components/OptionButton";
 import { colors, typography, spacing, layout } from "@/constants/Theme";
 import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamList } from '@/types/navigation';
+import { RootStackParamList } from '@/types/types';
 
 type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'AssistLevel'>;
 type RouteProps = RouteProp<RootStackParamList, 'AssistLevel'>;
